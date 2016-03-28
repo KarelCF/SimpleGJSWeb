9e3993d75e23c8da1385c01202c849f8d36532d5
IceMoka <wangshuai728@163.com>
2016-03-28 10:40:21 +0800

    0328-1

--- index.php
+++ index.php
@@ -30,17 +30,6 @@
     <script type="text/javascript" src="http://7xs5ai.com1.z0.glb.clouddn.com/plugins.js"></script>
     <script type="text/javascript" src="asset/js/main.js"></script>
 
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-75128097-2', 'auto');
-        ga('send', 'pageview');
-
-    </script>
-
 	<?php
 		if ($locale != "zh") {
 	?>
@@ -67,7 +56,7 @@
 
 </head>
 
-<body class="stretched">
+<body class="stretched"><?php include_once("analyticstracking.php") ?>
 
 <div id="wrapper" class="clearfix">
 
